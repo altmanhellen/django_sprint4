3d989ce0509e5fd0aa5dc973c3a46e135b3c2eca
Elena Altman <altmanhellen@gmail.com>
2024-01-05 15:28:56 +0200

    Group urls with similar prefix in urls.py

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
@@ -6,51 +6,55 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.IndexListView.as_view(), name='index'),
-    path(
-        'posts/create/',
-        views.PostCreateView.as_view(),
-        name='create_post'
-    ),
-    path(
-        'posts/<int:post_id>/',
-        views.PostDetailView.as_view(),
-        name='post_detail'
-    ),
-    path(
-        'posts/<int:post_id>/edit/',
-        views.PostUpdateView.as_view(),
-        name='edit_post'
-    ),
-    path(
-        'posts/<int:post_id>/delete/',
-        views.PostDeleteView.as_view(),
-        name='delete_post'
-    ),
-    path(
-        'posts/<int:post_id>/edit_comment/<int:pk>/',
-        views.CommentUpdateView.as_view(),
-        name='edit_comment'
-    ),
-    path(
-        'posts/<int:post_id>/delete_comment/<int:pk>/',
-        views.CommentDeleteView.as_view(),
-        name='delete_comment'
-    ),
-    path(
-        'posts/<int:post_id>/comment/',
-        views.CommentCreateView.as_view(),
-        name='add_comment'
-    ),
-    path(
-        'profile/edit/',
-        views.ProfileUpdateView.as_view(),
-        name='edit_profile'
-    ),
-    path(
-        'profile/<str:username>/',
-        views.ProfileListView.as_view(),
-        name='profile'
-    ),
+    path('posts/', include([
+        path(
+            'create/',
+            views.PostCreateView.as_view(),
+            name='create_post'
+        ),
+        path(
+            '<int:post_id>/',
+            views.PostDetailView.as_view(),
+            name='post_detail'
+        ),
+        path(
+            '<int:post_id>/edit/',
+            views.PostUpdateView.as_view(),
+            name='edit_post'
+        ),
+        path(
+            '<int:post_id>/delete/',
+            views.PostDeleteView.as_view(),
+            name='delete_post'
+        ),
+        path(
+            '<int:post_id>/edit_comment/<int:pk>/',
+            views.CommentUpdateView.as_view(),
+            name='edit_comment'
+        ),
+        path(
+            '<int:post_id>/delete_comment/<int:pk>/',
+            views.CommentDeleteView.as_view(),
+            name='delete_comment'
+        ),
+        path(
+            '<int:post_id>/comment/',
+            views.CommentCreateView.as_view(),
+            name='add_comment'
+        ),
+    ])),
+    path('profile/', include([
+        path(
+            'edit/',
+            views.ProfileUpdateView.as_view(),
+            name='edit_profile'
+        ),
+        path(
+            '<slug:username>/',
+            views.ProfilePostListView.as_view(),
+            name='profile'
+        ),
+    ])),
     path(
         'category/<slug:category_slug>/',
         views.CategoryPostListView.as_view(),
