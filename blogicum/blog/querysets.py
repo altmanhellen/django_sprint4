691e2ed6c1beb18d50b3aba9aaee5e2b92f6b57f
Elena Altman <altmanhellen@gmail.com>
2024-01-05 15:23:36 +0200

    Transfer query set filtration from utils.py to querysets.py

--- blogicum/blog/querysets.py
+++ blogicum/blog/querysets.py
@@ -1,8 +1,8 @@
-from django.db.models import QuerySet
+from django.db import models
 from django.utils import timezone
 
 
-class FilteredQuerySet(QuerySet):
+class FilteredQuerySet(models.QuerySet):
 
     def is_published(self):
         return self.filter(
@@ -10,3 +10,15 @@ class FilteredQuerySet(QuerySet):
             pub_date__lte=timezone.now(),
             category__is_published=True
         )
+
+
+class PersonManager(models.Manager):
+
+    def get_custom_queryset(self):
+        return FilteredQuerySet(self.model, using=self._db)
+
+    def get_post_list(self, category=None):
+        queryset = self.get_custom_queryset().is_published()
+        if category:
+            queryset = queryset.filter(category=category)
+        return queryset
